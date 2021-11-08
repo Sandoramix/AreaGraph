00c29f9e18ac1365af30d09ece5cf370343ea67a
sandoramix <sandoramixer61@gmail.com>
2021-11-08 18:54:53 +0100

    rest-api tests

--- Backend/rest-api-test.py
+++ Backend/rest-api-test.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, make_response
+from flask import Flask, abort
 from flask_restful import Api, Resource, reqparse
 
 app = Flask(__name__)
@@ -19,26 +19,29 @@ getparse.add_argument(
 
 
 postparse = reqparse.RequestParser()
-postparse.add_argument(
-    "name", type=str, help="'name' parameter not found/valid", required=True)
+postparse.add_argument("name", type=str, help="'name' parameter not found/valid", required=True)
 postparse.add_argument("age", type=int, help="'age' argument not found/valid", required=True)
+
+
 
 
 class StationData(Resource):
     def get(self):
         args = getparse.parse_args()
         name = args['name']
-        if name in names:
-            return names[name], 201
-        return {
-            "message": "No user found",
-        }, 404
+        self.name_exists(name)
+        return names[name], 201
+
 
 
     def post(self):
         args = postparse.parse_args()
+        age=int(args['age'])
+
+        if age <0:
+            abort(400,"Invalid age input [need to be int8]")
         names[args['name']] = {
-            "age":args['age']
+            "age":age
         }
         return {
             "message": "User added",
@@ -46,6 +49,10 @@ class StationData(Resource):
                 args['name']:names[args['name']]
             },
         },200
+
+    def name_exists(self,name):
+        if name not in names:
+            abort(400,f'user `{name}` not found')
 
 
 
