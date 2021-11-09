cb78acd18dd5591dadd9a8ab82e9e3075f5f0541
sandoramix <sandoramixer61@gmail.com>
2021-11-09 19:27:37 +0100

    added docs

--- Backend/main.py
+++ Backend/main.py
@@ -1,12 +1,16 @@
-from website import app,api,db,cur
+
+from website import app,api,db,cur,execute_query
+
+#http server module
 from gevent.pywsgi import WSGIServer
 
 
-
-
 if __name__=="__main__":
+    #dev.
     #app.run(debug=True,port=8080)
-    server=WSGIServer(('0.0.0.0',8080),app)
+    #production
+    server=WSGIServer(('0.0.0.0',7780),app)
     server.serve_forever()
+    
     db.close()
     cur.close()
