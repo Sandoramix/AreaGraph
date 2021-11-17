ff5dc1492b21d92b94e93612c7546b513b1beaf5
sandoramix <sandoramixer61@gmail.com>
2021-11-17 15:52:32 +0100

    update

--- Frontend/src/utils/Chart-class.ts
+++ Frontend/src/utils/Chart-class.ts
@@ -3,33 +3,79 @@ import { EChartsOption } from 'echarts';
 export class Chart_custom {
 	private x_type: 'category' | 'log' | 'time' | 'value' = 'category';
 
-	newLineChart(x_vals?: string[], y_vals?: number[], unit?: string): EChartsOption {
+	newLineChart(x_vals?: string[], y_vals?: number[], unit: string = ''): EChartsOption {
 		return {
-			xAxis: {
-				type: this.x_type,
-				boundaryGap: false,
-				data: x_vals ? x_vals : [],
-			},
-			yAxis: {
-				type: 'value',
-			},
-			tooltip: {
-				trigger: 'item',
-				showDelay: 0,
-				transitionDuration: 0.2,
-				formatter: function (params: any) {
-					return `<b>${params['name']}</b> : ${params['value']}${unit ? unit : ''}`;
+			...this.xAxisPart(x_vals ? x_vals : []),
+
+			...this.yAxisPart(unit),
+			...this.seriesPart(y_vals ? y_vals : []),
+			...this.tooltipPart(unit),
+			toolbox: {
+				feature: {
+					dataZoom: {
+						yAxisIndex: 'none',
+					},
+					restore: {},
+					saveAsImage: {},
 				},
 			},
-			series: {
-				data: y_vals ? y_vals : [],
-				type: 'line',
-				areaStyle: {},
+			dataZoom: [
+				{
+					type: 'inside',
+					start: 0,
+					end: 100,
+				},
+
+				{
+					start: 0,
+					end: 100,
+				},
+			],
+		};
+	}
+
+	mergedSeriesData(x: string[], y: number[], unit: string) {
+		return {
+			...this.xAxisPart(x),
+			...this.yAxisPart(unit),
+			...this.seriesPart(y),
+			...this.tooltipPart(unit),
+		};
+	}
+
+	private tooltipPart(unit: string): EChartsOption {
+		return {
+			tooltip: {
+				trigger: 'axis',
+				showDelay: 0,
+				transitionDuration: 0.2,
+				position: function (pos, params, dom, rect, size) {
+					return [size.viewSize[0] - pos[0] > size.viewSize[0] * 0.3 ? pos[0] : '70%', '10%'];
+				},
+				formatter: function (params: any) {
+					let data = params[0];
+					return `<div style="text-align:center;width:15em">
+						<b>${data.name}</b>
+						<br>
+						${data.value} ${unit}
+					</div>`;
+				},
 			},
 		};
 	}
 
-	mergedXAxis(x: string[]) {
+	private seriesPart(y: number[]): EChartsOption {
+		return {
+			series: {
+				data: y,
+				type: 'line',
+
+				areaStyle: {},
+				smooth: true,
+			},
+		};
+	}
+	private xAxisPart(x: string[]): EChartsOption {
 		return {
 			xAxis: {
 				type: this.x_type,
@@ -38,20 +84,17 @@ export class Chart_custom {
 			},
 		};
 	}
-	mergedSeriesData(y: number[], unit: string) {
+
+	private yAxisPart(unit: string): EChartsOption {
 		return {
-			series: {
-				data: y,
-				type: 'line',
-				areaStyle: {},
-			},
-			tooltip: {
-				trigger: 'item',
-				showDelay: 0,
-				transitionDuration: 0.2,
-				formatter: function (params: any) {
-					return `<b>${params['name']}</b> : ${params['value']}${unit}`;
+			yAxis: {
+				type: 'value',
+				name: unit,
+				nameTextStyle: {
+					fontWeight: 'bold',
+					align: 'right',
 				},
+				boundaryGap: [0, '100%'],
 			},
 		};
 	}
