6bbb7542d776c05b8a7058a963ec8d2e064ec092
sandoramix <sandoramixer61@gmail.com>
2021-11-23 23:09:23 +0100

    heroku app files

---
+++ Heroku/main.py
@@ -0,0 +1,33 @@
+
+from backend import app
+from flask import send_from_directory
+
+# Frontend routes
+@app.route('/', methods=['GET'])
+def root():
+    return send_from_directory('../frontend', 'index.html')
+
+
+@app.route('/<path:path>', methods=['GET'])
+def static_proxy(path):
+    print(path)
+    return send_from_directory('../frontend', path)
+
+
+@app.route('/home', methods=['GET'])
+def home():
+    return send_from_directory('../frontend', 'index.html')
+
+
+@app.route('/about', methods=['GET'])
+def about():
+    return send_from_directory('../frontend', 'index.html')
+
+
+@app.route('/map', methods=['GET'])
+def map():
+    return send_from_directory('../frontend', 'index.html')
+
+
+if __name__ == "__main__":
+    app.run()
