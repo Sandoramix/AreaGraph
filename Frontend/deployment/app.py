0a0f2d17c454fa6d1e91c46752485072f7005959
sandoramix <sandoramixer61@gmail.com>
2021-11-23 11:55:05 +0100

    heroku flask app update

--- Frontend/deployment/app.py
+++ Frontend/deployment/app.py
@@ -1,3 +1,4 @@
+from os import truncate
 from flask import Flask, send_from_directory
 from flask_cors import CORS
 
@@ -7,11 +8,27 @@ cors = CORS(app, send_wildcard=True)
 
 @app.route('/<path:path>', methods=['GET'])
 def static_proxy(path):
+    print(path)
     return send_from_directory('./website', path)
 
 
 @app.route('/')
 def root():
+    return send_from_directory('./website', 'index.html')
+
+
+@app.route('/home')
+def home():
+    return send_from_directory('./website', 'index.html')
+
+
+@app.route('/about')
+def about():
+    return send_from_directory('./website', 'index.html')
+
+
+@app.route('/map')
+def map():
     return send_from_directory('./website', 'index.html')
 
 
