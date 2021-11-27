c5ba8516481dfbb18468b0445bd5a7ddd9fc0ff5
sandoramix <sandoramixer61@gmail.com>
2021-11-27 19:27:18 +0100

    app component

--- Frontend/src/app/app.component.ts
+++ Frontend/src/app/app.component.ts
@@ -1,10 +1,11 @@
-import { TitleManagementService } from './services/title/title-management.service';
-import { Component, OnInit } from '@angular/core';
+import { HttpRequestService } from "src/app/services/requests/http-request.service";
+import { TitleManagementService } from "./services/title/title-management.service";
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-	selector: 'app-root',
-	templateUrl: './app.component.html',
-	styleUrls: ['./app.component.css'],
+	selector: "app-root",
+	templateUrl: "./app.component.html",
+	styleUrls: ["./app.component.css"],
 })
 export class AppComponent implements OnInit {
 	active_route: string;
