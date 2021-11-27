a1e6ba668530102aa672a891ec66c1599b7b130c
sandoramix <sandoramixer61@gmail.com>
2021-11-27 14:00:06 +0100

    merged Station interfaces to a single file

--- Frontend/src/utils/Station.ts
+++ Frontend/src/utils/Station.ts
@@ -1,6 +1,19 @@
+import { Sensor } from "./Sensor";
 export interface Station {
 	id: number;
 	name: string;
 	latitude: number;
 	longitude: number;
+	date_from: string;
+	date_to: string;
+}
+
+export interface StationAvg {
+	station: Station;
+	data_hourly_avg: StationHourlyAvg[];
+}
+export interface StationHourlyAvg {
+	created_on: string;
+	avg_value: number;
+	sensor: Sensor;
 }
--- Frontend/src/utils/StationAvg.ts
+++
@@ -1,7 +0,0 @@
-import { Station } from './Station';
-import { StationHourlyAvg } from './StationHourlyAvg';
-
-export interface StationAvg {
-	station: Station;
-	data_hourly_avg: StationHourlyAvg[];
-}
